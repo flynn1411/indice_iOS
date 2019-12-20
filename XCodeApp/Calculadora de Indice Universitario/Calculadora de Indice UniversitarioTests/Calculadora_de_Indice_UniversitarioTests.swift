2f89eda555045b21348b3d2be405bd8d068e48f6
Josué Izaguirre <flynn1411@gmail.com>
2019-12-19 19:50:24 -0600

    App en Swift Commit

---
+++ XCodeApp/Calculadora de Indice Universitario/Calculadora de Indice UniversitarioTests/Calculadora_de_Indice_UniversitarioTests.swift
@@ -0,0 +1,34 @@
+//
+//  Calculadora_de_Indice_UniversitarioTests.swift
+//  Calculadora de Indice UniversitarioTests
+//
+//  Created by Josué Izaguirre on 12/19/19.
+//  Copyright © 2019 Josué Izaguirre. All rights reserved.
+//
+
+import XCTest
+@testable import Calculadora_de_Indice_Universitario
+
+class Calculadora_de_Indice_UniversitarioTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ XCodeApp/Calculadora de Indice Universitario/Calculadora de Indice UniversitarioUITests/Calculadora_de_Indice_UniversitarioUITests.swift
@@ -0,0 +1,43 @@
+//
+//  Calculadora_de_Indice_UniversitarioUITests.swift
+//  Calculadora de Indice UniversitarioUITests
+//
+//  Created by Josué Izaguirre on 12/19/19.
+//  Copyright © 2019 Josué Izaguirre. All rights reserved.
+//
+
+import XCTest
+
+class Calculadora_de_Indice_UniversitarioUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
