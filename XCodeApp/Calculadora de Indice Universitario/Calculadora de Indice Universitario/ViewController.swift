d8b5401481e86e90e584238dbfb19c2fcc15101a
Josué Izaguirre <flynn1411@gmail.com>
2019-12-19 20:29:25 -0600

    Estructura de la Interfaz editada commit

---
+++ XCodeApp/Calculadora de Indice Universitario/Calculadora de Indice Universitario/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculadora de Indice Universitario
+//
+//  Created by Josué Izaguirre on 12/19/19.
+//  Copyright © 2019 Josué Izaguirre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
